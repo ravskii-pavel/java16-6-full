e48459e71ed8629400937c1da5872c04e45b1a3f
Pavel Ravskiy <ravskiy@dn.shambala>
2016-12-05 19:37:32 +0200

    HW09

---
+++ Classwork/lesson9/src/Calc.java
@@ -0,0 +1,73 @@
+/**
+ * Created by java on 02.12.2016.
+ */
+public class Calc {
+
+    public static int sum(int num1, int num2) {
+
+        int result = num1 + num2;
+        return result;
+    }
+
+    public static float sum(float num1, float num2) {
+
+        float result = num1 + num2;
+        return result;
+    }
+
+    public static int sum(String num1, String num2) {
+
+        int result = Integer.parseInt(num1) + Integer.parseInt(num2);
+        return result;
+    }
+
+    public static int diff(int num1, int num2) {
+
+        int result = num1 - num2;
+        return result;
+    }
+    public static float diff(float num1, float num2) {
+
+        float result = num1 - num2;
+        return result;
+    }
+    public static int diff(String num1, String num2) {
+
+        int result = Integer.parseInt(num1) + Integer.parseInt(num2);
+        return result;
+    }
+
+    public static int div(String num1, String num2) {
+
+        int result = Integer.parseInt(num1) / Integer.parseInt(num2);
+        return result;
+    }
+
+    public static float div(float num1, float num2) {
+
+        float result = num1 / num2;
+        return result;
+    }
+    public static int div(int num1, int num2) {
+
+        int result = num1 / num2;
+        return result;
+    }
+    public static int multiplication(int num1, int num2) {
+
+        int result = num1 * num2;
+        return result;
+    }
+    public static float multiplication(float num1, float num2) {
+
+        float result = num1 * num2;
+        return result;
+    }
+    public static int multiplication(String num1, String num2) {
+
+        int result = Integer.parseInt(num1) * Integer.parseInt(num2);
+        return result;
+    }
+
+}
+
