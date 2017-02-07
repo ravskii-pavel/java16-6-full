0098ac2fdd6fc90d010a36a291301b8a7b773edc
Ravskiy Pavel <ravskii.pavel@gmail.com>
2017-02-07 18:06:44 +0200

    hw23++

--- Homework/homework_23/src/Main.java
+++ Homework/homework_23/src/Main.java
@@ -6,14 +6,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //System.out.println("Hello World!");
+        //Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //String[] arrayS = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         //List list = Arrays.asList(array);
-        ArrayList<Integer>list1 = new ArrayList<>(Arrays.asList(array));
-        Integer myArray[] = list1.toArray(new Integer[list1.size()]);
+        //ArrayList<Integer>list1 = new ArrayList<>(Arrays.asList(array));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i =1; i <=10; i++){
+            list.add(i);
+        }
+
+        Integer res[] = Arrays.copyOf(list.toArray(new Integer[list.size()]), 5);
+
+
+
+        //Integer[] res = (Arrays.copyOf(myArray, 5));
+        //ArrayList <Integer>list2 = new ArrayList<>(Arrays.asList(res));
+
+       /* Integer myArray[] = list1.toArray(new Integer[list1.size()]);
         Integer[] res = (Arrays.copyOf(myArray, 5));
-        ArrayList <Integer>list2 = new ArrayList<>(Arrays.asList(res));
+        ArrayList <Integer>list2 = new ArrayList<>(Arrays.asList(res));*/
 
         //ArrayList list1 = new ArrayList<>(Arrays.asList(arrayS));
         //ArrayList<Integer> arrayList = new ArrayList<>(list);
--- Homework/homework_23/src/collections/ArraysTest.groovy
+++ Homework/homework_23/src/collections/ArraysTest.groovy
@@ -1,4 +1,5 @@
 package collections
+import spock.lang.Specification
 import java.util.ArrayList;
 /*
 import java.util.Arrays;
@@ -8,30 +9,23 @@ import java.util.Collections;
 /**
  * Created by Ravskiy Pavel on 05.02.2017.
  */
-class ArraysTest extends spock.lang.Specification {
+class ArraysTest extends Specification {
 
     def "copyOf() test1"() {
+        //Integer res[] =  new Integer();
+        ArrayList<Integer> list = new ArrayList<>();
 
-        Integer[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        //List list = Arrays.asList(array);
-        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(array));
-        //ArrayList<Integer> arrayList = new ArrayList<>(list);
-
-        /*ArrayList l1 = new ArrayList<>();
-        ArrayList res = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            l1.add(i);
+        for (int i =0; i <  10; i++){
+            list.add(i);
         }
-*/
-
-      when:
-
-      ArrayList res = Arrays.copyOf(list1,4);
 
 
-        then:
-        res == [0, 1, 2, 3];
+    when:
+      Integer[] res = Arrays.copyOf(list.toArray(new Integer[list.size()]), 5);
 
+
+    then:
+      res == [0, 1, 2, 3, 4];
     }
 
 
