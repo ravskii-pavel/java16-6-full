278c9654e4c4ed24d95277f3c2a626c5831d4e69
Pavel <ravskii.pavel@gmail.com>
2017-02-07 00:17:21 +0000

    he23++

--- Homework/homework_23/src/Main.java
+++ Homework/homework_23/src/Main.java
@@ -11,9 +11,13 @@ public class Main {
         //String[] arrayS = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         //List list = Arrays.asList(array);
         ArrayList<Integer>list1 = new ArrayList<>(Arrays.asList(array));
+        Integer myArray[] = list1.toArray(new Integer[list1.size()]);
+        Integer[] res = (Arrays.copyOf(myArray, 5));
+        ArrayList <Integer>list2 = new ArrayList<>(Arrays.asList(res));
+
         //ArrayList list1 = new ArrayList<>(Arrays.asList(arrayS));
         //ArrayList<Integer> arrayList = new ArrayList<>(list);
 
-         res =
+
     }
 }
