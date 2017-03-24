811f6c6662d32013127b7aabfc05c582ac1d1d96
java <java@training.workrocks.ua>
2017-03-24 20:55:05 +0200

    classWork_24.03.17

--- Homework/homework_36_Additional/src/main/java/CitizenDAOImpl.java
+++ Homework/homework_36_Additional/src/main/java/CitizenDAOImpl.java
@@ -7,16 +7,23 @@ public class CitizenDAOImpl implements DAO {
         this.connection = connection;
     }
 
-    public void create(String first_name, String email, String password) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO citizen (first_name, last_name, age, street_id) VALUES (?, ?, ?, ?)");
-        Citizen citizen = new Citizen(user_name, email, password);
+    public void create(String first_name, String last_name, int age, Long street_id) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement
+                ("INSERT INTO citizen (first_name, last_name, age, street_id) " +
+                        "VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        Citizen citizen = new Citizen(first_name, last_name, age, street_id);
 
         preparedStatement.setString(1, citizen.getFirst_name());
+
         preparedStatement.setString(2, citizen.getLast_name());
         preparedStatement.setLong(3, citizen.getAge());
         preparedStatement.setLong(4, citizen.getStreet_id());
 
         preparedStatement.execute();
+    }
+
+    public void create(String s1, String s2, String s3) throws SQLException {
+
     }
 
     public void update(int id, String s1) {
@@ -27,8 +34,13 @@ public class CitizenDAOImpl implements DAO {
 
     }
 
-    public void update(int id_new, String user_name, String email, String password) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE citizen SET (first_name = ?, last_name = ?, age = ?, street_id = ?) WHERE id = ?");
+    public void update(int id, String s1, String s2, String s3) throws SQLException {
+
+    }
+
+    /*public void update(int id_new, String user_name, String email, String password) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE citizen " +
+                "SET (first_name = ?, last_name = ?, age = ?, street_id = ?) WHERE id = ?");
         Citizen citizen = new Citizen(user_name, email, password);
 
         preparedStatement.setString(1, citizen.getUser_name());
@@ -36,7 +48,7 @@ public class CitizenDAOImpl implements DAO {
         preparedStatement.setString(3, citizen.getPassword());
         preparedStatement.setInt(4, id_new);
         preparedStatement.executeUpdate();
-    }
+    }*/
 
     public void delete() {
 
