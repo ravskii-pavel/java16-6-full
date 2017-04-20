bcc0e96e7529457eb2673e59c86fd4c98e39eb5c
Ravskiy Pavel <ravskii.pavel@gmail.com>
2017-04-20 19:05:31 +0300

    HW_43_

--- Homework/homework_43/src/Main.java
+++ Homework/homework_43/src/Main.java
@@ -3,11 +3,15 @@ import threads.MyThread;
 public class Main {
 
     public static void main(String[] args) {
-        MyThread myThread = new MyThread();
+        /*MyThread myThread = new MyThread();
         MyThread myThread1 = new MyThread();
         myThread.start();
-        myThread1.start();
+        myThread1.start();*/
 
+        MyThread myThread3 = new MyThread();
+        myThread3.printHelloNew(7);
+        myThread3.start();
+        //printHello(5);
 
 
       /*  Thread thread = new Thread(){
@@ -24,5 +28,12 @@ public class Main {
        for(int i =0; i< 10; i++){
             System.out.println(Thread.currentThread().getName() + " " + i);
         }*/
+    }
+    public static void printHello(int count){
+
+        for (int i = 0; i < count; i++){
+            new MyThread().start();
+        }
+
     }
 }
--- Homework/homework_43/src/threads/MyThread.java
+++ Homework/homework_43/src/threads/MyThread.java
@@ -7,12 +7,17 @@ public class MyThread extends Thread {
     public MyThread() {
         this.name = "Hello World";
     }*/
+    public void printHelloNew(int count){
+        for (int i = 0; i < count; i++){
+            new MyThread().start();
+        }
+    }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++){
-            //System.out.println(Thread.currentThread().getName() + " " + i);
+        /*for (int i = 0; i < 10; i++){
+            //System.out.println(Thread.currentThread().getName() + " " + i);*/
             System.out.println(Thread.currentThread().getName() + " " + "Hello World");
-        }
+        //}
     }
 }
