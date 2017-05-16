712a4422d13a8e3b33968407fe099e2f5b0b5fae
Ravskiy Pavel <ravskii.pavel@gmail.com>
2017-05-16 15:36:27 +0300

    HW_46_Full

--- Classwork/lesson46_new/HomeServlet.java
+++ Classwork/lesson46_new/HomeServlet.java
@@ -15,11 +15,27 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        PrintWriter writer = resp.getWriter();
+//        writer.println("<h1>Hello World!</h1>");
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*String name = req.getParameter("login");
+        req.setAttribute("userName", name);*/
+
+//        PrintWriter writer = resp.getWriter();
+//        writer.println(String.format("Hello, %s! Your password length is: %d", login, password.length()));
+
+        //req.setAttribute("userName", login);
+        //req.setAttribute("balance", "100$");
+        /*if(req.getParameter("sign in").equals("Sign in")){
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+        else if(req.getParameter("registration").equals("Registration")){
+            req.getRequestDispatcher("registration.jsp").forward(req, resp);
+        }*/
         req.getRequestDispatcher("userPage.jsp").forward(req, resp);
     }
 }
