c9050c928efc9f8a0d4db330c59a7bf45f5f3387
Pavel <ravskii.pavel@gmail.com>
2017-05-22 01:55:20 +0100

    +47HW

---
+++ Homework/homework_47/src/main/java/web/HomeServlet.java
@@ -0,0 +1,27 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by java on 12.05.2017.
+ */
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("userPage.jsp").forward(req, resp);
+    }
+}
---
+++ Homework/homework_47/src/main/java/web/dao/DataProvider.java
@@ -0,0 +1,11 @@
+package web.dao;
+
+/**
+ * Created by Ravskiy Pavel on 21.05.2017.
+ */
+public interface DataProvider {
+
+    enum ConnectionType {
+        MYSQL, JSON
+    }
+}
